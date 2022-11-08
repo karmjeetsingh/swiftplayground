fa751dd20e5806c58dd70071b01bd5e91333dd8e
Karmjeet Singh <karmjeetsingh216@gmail.com>
2022-11-08 05:28:51 -0500

    basic playground added

---
+++ Basics.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+//Data types
+let tureFalse: Bool = true
+let fraction: Float = 2.0
+let longLength: Double = 12345
+let number: Int = 100
+let chars: String = "Waheguru"
+// Array, Set, Dictionary --> Collection type
+// Tuple -> familiar type. can return grouping of value
+// Optional type --> There is a value or not at all
+var changeValue: Int = 5
+let maximumLoginAttemp = 10
+var currentLoginAttemp = 0
+
+//We can declar multiple type at single line
+var x = 1.0, y = 3.4, z = 21.1
+
+// Type annotation . declaring data type
+var welcomeMessage: String
