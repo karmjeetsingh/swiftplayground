e592181e462a34653010139c1294301969e67c7c
Karmjeet Singh <karmjeetsingh216@gmail.com>
2022-11-08 23:07:41 -0500

    numeric conversion

--- Basics.playground/Contents.swift
+++ Basics.playground/Contents.swift
@@ -26,3 +26,69 @@ welcomeMessage = "Welcome to USA"
 var  red, pink, green: Double
 
 // type safe and type inference
+var languageName = "Swift"
+    languageName = "Swift++"
+
+print(languageName)
+print(languageName, terminator: "   XXX    ")
+print(languageName)
+// So print function has defualt paramenter ( separator and terminator)
+// where terminator has default value of line break . if you don't mention it use that.
+
+//MARK: STRING INTERPOLATION
+let age: Int = 33
+print("Age of Ravi Saini is : \(age)")
+
+//MARK: COMMENTS are non executive able text in code
+ // single line
+/*
+ Multi line
+ comments
+ */
+// Nested Multiline comments
+
+/*
+ Heading
+    /* Body */
+ End
+ */
+
+//MARK: Semicolon
+// not required but we can use after end of statement.
+    // only required when multiple statemnet in same line
+
+let cat = "catty"; var dog = "doggy"; print(cat,    separator:" ",  terminator: dog); print(" \(cat)  loves \(dog)")
+
+//MARK: Integer Inbound
+let minValue = UInt.min
+let maxValue = Int.max
+print("Min is :\(minValue)  Max is : ", separator:"",terminator: String(maxValue))
+print("")
+let max64bit = UInt64.max
+print(max64bit)
+// so if we only specify Int , swift automatically set it according to platform either 32 or 64
+
+// Better to use Uint and Int  when we need spcific size number , becuae they have charatics of 32 and 64 as well
+
+//MARK: Floating
+// 32 bit is floting point number use Float 6 decicimal digits
+// 64 bit use Double . 15 decimal digits
+
+//MARK: Type safe, Type check , Interfence can infer any data type from data
+// perfom type checks
+let attemps = 5; let pi = 3.14
+// swift automatically infer the data type for two contants  Int and Double
+// by default consider Double to fraction number or decimal number
+
+//MARK: Number system
+let decimalInteger = 17
+let binaryInterger = 0b10001
+let octalInteger = 0o21   //17
+let hexadecimalInteger = 0x11 //17
+ 
+
+//MARK: Exponentional or Exponent
+// 1.25e2  = 1.25 * 10 power 2 or 125.0
+//1.25e-2  = 1.25 * 10 power -2 or 0.0125
+
+// --> Any number signed and Unsigned called Litral
