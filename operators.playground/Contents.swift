6c215837c13868b7897b23fd6f14b3dc85925714
Karmjeet Singh <karmjeetsingh216@gmail.com>
2022-11-13 12:16:41 -0500

    working on operator

--- operators.playground/Contents.swift
+++ operators.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
 var greeting = "Basic Operators"
+// Unary, Binary, Ternary
+//Arithmetic(+,-,*,/,%), Logical(&& AND, ||OR ) , Range ( a...b, a..<b )
+
+//Unary:  Prefix !b, -a , Postfix C!
+//Binary  a + 2
+// Ternary  ( a ? b : C )
+
+//MARK:  assignment operator
+var a = 10
+var b = 4
+a = a + b
+b = a - b
+a = a - b
+print(a,b)
+
+var (x, y) = (1, 1)
+
+if x == y {
+    print("lol")
+}
